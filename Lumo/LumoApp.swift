1c556054f84bb1217efd5060b51ece918f1b9853
Anthony Getsin <anthony.getsin@gmail.com>
2025-06-16 12:48:20 -0700

    Initial Commit

---
+++ Lumo/LumoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumoApp.swift
+//  Lumo
+//
+//  Created by Tony on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct LumoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
